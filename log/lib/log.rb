c25feab0e2569732a44224be2f9fa784d352d83b
Matt Parker <moonmaster9000@gmail.com>
2014-07-20 10:06:19 -0700

    log library for removing cross-cutting logging concern from code

---
+++ log/lib/log.rb
@@ -0,0 +1,28 @@
+require "logger"
+
+class Log
+  def initialize(logger: Logger.new(STDOUT), logs: [])
+    @logger = logger
+    @logs = logs
+  end
+
+  def start
+    @logs.each do |log|
+      log = log.new
+
+      logger = @logger
+
+      log.klass.instance_eval do
+        define_method("logged_#{log.method_to_log}") do |*args|
+          log.before(logger: logger, args: args) if log.respond_to?(:before)
+          return_value = send("original_#{log.method_to_log}", *args)
+          log.after(logger: logger, args: args) if log.respond_to?(:after)
+          return_value
+        end
+
+        alias_method "original_#{log.method_to_log}", log.method_to_log
+        alias_method log.method_to_log, "logged_#{log.method_to_log}"
+      end
+    end
+  end
+end
---
+++ log/spec/log_spec.rb
@@ -0,0 +1,43 @@
+require "log"
+
+describe Log do
+  it "hooks into methods before and after they're run" do
+    run_order = []
+
+    klass_to_log = Class.new do
+      define_method(:some_method) do |*args|
+        run_order << [:some_method, args]
+      end
+    end
+
+    log = Class.new do
+      define_method(:klass) do
+        klass_to_log
+      end
+
+      def method_to_log
+        :some_method
+      end
+
+      define_method(:before) do |logger: nil, args: []|
+        run_order << [:before, logger, args]
+      end
+
+      define_method(:after) do |logger: nil, args: []|
+        run_order << [:after, logger, args]
+      end
+    end
+
+    logger = double(:logger)
+    Log.new(logger: logger, logs: [log]).start
+
+    arg = double(:arg)
+    klass_to_log.new.some_method(arg)
+
+    expect(run_order).to eq [
+      [:before, logger, [arg]],
+      [:some_method, [arg]],
+      [:after, logger, [arg]],
+    ]
+  end
+end

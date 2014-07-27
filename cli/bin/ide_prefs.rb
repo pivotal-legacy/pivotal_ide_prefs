a70cf180b6009680ff2a869f050de6d28aca257d
Matt Parker <moonmaster9000@gmail.com>
2014-07-27 08:31:41 -0700

    You can now specify the log level at the command line.

--- cli/bin/ide_prefs.rb
+++ cli/bin/ide_prefs.rb
@@ -11,30 +11,36 @@ require "ide_prefs"
 require "persistence"
 require "cli"
 
-options = {}
+repo_config_options = {}
+logging_options = {log_level: :info}
 
 OptionParser.new do |opts|
   opts.on("--ide=IDE") do |ide|
-    options[:user_prefs_repo_location] = Module.const_get("Cli::Ide::#{ide.capitalize}").new.user_prefs_repo_location
-    options[:ide_name] = ide
+    repo_config_options[:user_prefs_repo_location] = Module.const_get("Cli::Ide::#{ide.capitalize}").new.user_prefs_repo_location
+    repo_config_options[:ide_name] = ide
   end
 
   opts.on("--user-prefs-location=PATH") do |location|
-    options[:user_prefs_repo_location] = location
+    repo_config_options[:user_prefs_repo_location] = location
   end
 
-
   opts.on("--backup-prefs-location=PATH") do |location|
-    options[:backup_prefs_repo_location] = location
+    repo_config_options[:backup_prefs_repo_location] = location
+  end
+
+  opts.on("--log-level=LEVEL", [:info, :debug, :silent]) do |level|
+    logging_options[:log_level] = level
   end
 end.parse!
 
-if options[:ide_name].nil?
+if repo_config_options[:ide_name].nil?
   puts "You must specify an IDE with the --ide switch. Use the `--help` switch for more information."
   exit 1
 end
 
-repo_configuration = Cli::Configuration::RepoConfiguration.new(options)
+Cli::Logger.new(log_level: logging_options[:log_level]).start
+
+repo_configuration = Cli::Configuration::RepoConfiguration.new(repo_config_options)
 
 repos = {
   user_prefs_repo:    Persistence::Repos::UserPrefsRepo.new(location: repo_configuration.user_prefs_repo_location),
--- cli/lib/cli/logging.rb
+++ cli/lib/cli/logging.rb
@@ -7,11 +7,23 @@ Dir[File.expand_path(File.join(__dir__, "logging", "*.rb"))].each do |log_file|
   logs << Module.const_get("Cli::Logging::#{File.basename(log_file).gsub(".rb", "").split("_").map(&:capitalize).join("")}")
 end
 
-logger = Logger.new(STDOUT)
-logger.progname = "pivotal_ide_prefs"
-logger.level = Logger::Severity::INFO
-logger.formatter = proc do |severity, datetime, progname, msg|
-  "#{progname} #{datetime}: #{severity} - #{msg}\n"
-end
+require "cli"
 
-Log.new(logs: logs, logger: logger).start
+Cli::Logger = Class.new do
+  LOGS = logs
+
+  def initialize(log_level: :info)
+    @log_level = log_level
+  end
+
+  def start
+    logger = Logger.new(STDOUT)
+    logger.progname = "pivotal_ide_prefs"
+    logger.level = Module.const_get("Logger::Severity::#{@log_level.upcase}")
+    logger.formatter = proc do |severity, datetime, progname, msg|
+      "#{progname} #{datetime}: #{severity} - #{msg}\n"
+    end
+
+    Log.new(logs: LOGS, logger: logger).start
+  end
+end

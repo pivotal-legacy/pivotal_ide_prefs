218a02c01c6440f58e7b93dcb4dc945b049cf8df
Matt Parker <moonmaster9000@gmail.com>
2014-07-27 21:29:13 -0700

    cleaning up cli logging setup

--- cli/lib/cli/logging.rb
+++ cli/lib/cli/logging.rb
@@ -1,29 +1,33 @@
 require "logging"
-
-logs = []
-
-Dir[File.expand_path(File.join(__dir__, "logging", "*.rb"))].each do |log_file|
-  require log_file
-  logs << Module.const_get("Cli::Logging::#{File.basename(log_file).gsub(".rb", "").split("_").map(&:capitalize).join("")}")
-end
-
 require "cli"
 
-Cli::Logger = Class.new do
-  LOGS = logs
+class Cli::Logger
+  LOGS = []
+
+  Dir[File.expand_path(File.join(__dir__, "logging", "*.rb"))].each do |log_file|
+    require log_file
+    LOGS.unshift Module.const_get("Cli::Logging::#{File.basename(log_file).gsub(".rb", "").split("_").map(&:capitalize).join("")}")
+  end
 
   def initialize(log_level: :info)
     @log_level = log_level
   end
 
   def start
-    logger = Logger.new(STDOUT)
-    logger.progname = "pivotal_ide_prefs"
-    logger.level = Module.const_get("Logger::Severity::#{@log_level.upcase}")
-    logger.formatter = proc do |severity, datetime, progname, msg|
-      "#{progname} #{datetime}: #{severity} - #{msg}\n"
-    end
-
     Logging.new(logs: LOGS, logger: logger).start
+  end
+
+  private
+  def logger
+    @logger ||= begin
+      logger = Logger.new(STDOUT)
+      logger.progname = "pivotal_ide_prefs"
+      logger.level = Module.const_get("Logger::Severity::#{@log_level.upcase}")
+      logger.formatter = proc do |severity, datetime, progname, msg|
+        "#{progname} #{datetime}: #{severity} - #{msg}\n"
+      end
+
+      logger
+    end
   end
 end

7f8dcc5b3b92e3289edba3f211915ddf519651ef
Chad <thewoolleyman@gmail.com>
2014-12-02 20:36:39 -0700

    WebStorm support

---
+++ cli/lib/cli/ide/webstorm.rb
@@ -0,0 +1,17 @@
+require "cli/ide/preferences_directory"
+
+module Cli
+  module Ide
+    class Webstorm
+      def user_prefs_repo_location
+        preferences_directory = File.expand_path(File.join("~", "Library", "Preferences"))
+
+        PreferencesDirectory.new(
+          directories:    Dir.glob(File.join(preferences_directory, "WebStorm*")),
+          match_pattern:  /WebStorm.*/,
+          default: File.join(preferences_directory, "WebStorm9"),
+        ).to_s
+      end
+    end
+  end
+end

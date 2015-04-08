6ced7e1a43fff7a6dd1a372845cb56bc81df144d
Anuj Biyani and Travis Grathwell <pair+abiyani+tgrathwell@pivotal.io>
2015-04-08 14:37:11 -0700

    Fix issue with deriving relative paths in Ruby 2.2
    When determining what files to create symlinks for, there is a 'sub'
    statement that peels off the root of a filepath to derive a relative
    path. This works in Ruby 2.1, because both of the paths happen to be
    lowercase, but a fix in Ruby 2.2 has broken it.
    
    Namely, if you had a file called "Filename" (note the case) this
    behavior changed:
     # (Ruby 2.1)
    Dir['filename'] #=> ['filename']
    
     # (Ruby 2.2)
    Dir['filename'] #=> ['Filename']
    
    Now, we ensure that the path for "pref_sources/RubyMine" has the correct
    case, AND change the path sub code to be case insensitive.

--- cli/lib/cli/configuration/repo_configuration.rb
+++ cli/lib/cli/configuration/repo_configuration.rb
@@ -19,11 +19,11 @@ class Cli::Configuration::RepoConfiguration
   end
 
   def backup_prefs_repo_location
-    @backup_prefs_repo_location || File.join(@backups_location_base, @ide_name)
+    @backup_prefs_repo_location || Dir[File.join(@backups_location_base, @ide_name)].first
   end
 
   def pivotal_prefs_repo_location
-    @pivotal_prefs_repo_location || File.join(@pivotal_prefs_location_base, @ide_name)
+    @pivotal_prefs_repo_location || Dir[File.join(@pivotal_prefs_location_base, @ide_name)].first
   end
 
   def user_prefs_repo_location
